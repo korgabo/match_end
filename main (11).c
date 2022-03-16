37e4704a5165db3200a5da2433d306474de1143c
korgabo <99967224+korgabo@users.noreply.github.com>
2022-03-16 21:51:35 +0100

    Add files via upload

---
+++ main (11).c
@@ -0,0 +1,37 @@
+/******************************************************************************
+
+                            Online C Compiler.
+                Code, Compile, Run and Debug C program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <stdio.h>
+#include "prog1.h"
+#include <string.h>
+
+
+int match_ends(int n, string words[]){
+    int egyezo = 0;
+    for(int i = 0; i < n; ++i){
+        string szo = words[i];
+            if ((strlen(szo) >= 2) && (szo[0] == szo[strlen(szo)-1])){
+            ++egyezo;
+            }
+        }
+    return egyezo;
+}
+
+int main()
+{
+    string szavak1[] = { "aba", "xyz", "aa", "x", "bbb" };
+    int szavak1_meret = 5;
+
+    printf("Az alábbi sztringekből összesen %d felel meg, a megadott feltételnek.", match_ends(szavak1_meret, szavak1));
+    for (int i = 0; i < szavak1_meret; ++i)
+    {
+        puts(szavak1[i]);
+    }
+
+    return 0;
+}
